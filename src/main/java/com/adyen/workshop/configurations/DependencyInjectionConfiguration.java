f261c018d1080f9d92dd6b8325cb70c6957ca101
kwokhe <kwokhe.chu@adyen.com>
2025-01-21 18:33:16 +0100

    Clean template

--- src/main/java/com/adyen/workshop/configurations/DependencyInjectionConfiguration.java
+++ src/main/java/com/adyen/workshop/configurations/DependencyInjectionConfiguration.java
@@ -20,8 +20,7 @@ public class DependencyInjectionConfiguration {
     Client client() {
         // Step 4
         var config = new Config();
-        config.setApiKey(applicationConfiguration.getAdyenApiKey()); // We now use the Adyen API Key
-        config.setEnvironment(Environment.TEST);		             // Sets the environment to TEST
+
         return new Client(config);
     }
 
